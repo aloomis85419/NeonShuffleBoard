d3f63e445484f2bb1a543c60a8fa33d4dd289ea3
aloomis85419 <aloomis85419@gmail.com>
2017-04-24 13:06:16 -0600

    Code seems to be in better condition. Still need to figure out how to properly reset and the distance calsculation is wonky after the dirst round :(

---
+++ app/src/main/java/com/example/aloom/neonshuffleboardv2/ScoreZoneFragment.java
@@ -0,0 +1,12 @@
+package com.example.aloom.neonshuffleboardv2;
+
+import android.app.Fragment;
+
+/**
+ * Created by aloom on 4/24/2017.
+ */
+
+public class ScoreZoneFragment extends Fragment {
+
+
+}
